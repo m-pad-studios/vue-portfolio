90eb3fa53acd15e3325a98c531b9cf844ebcf3cf
Mikail-Parkay <56673228+m-pad-studios@users.noreply.github.com>
2021-07-19 00:05:43 -0700

    -c

--- public/assets/js/menu-funcs.js
+++ public/assets/js/menu-funcs.js
@@ -1,10 +1,18 @@
 
-        function closeMenu() {
-            document.getElementById("bod").style.marginLeft = "-300px"
+   
+
+      
+!(function($) {
+    "use strict";
+
+    $(document).on('click', '#navvy', function(e) {
+
+ document.getElementById("bod").style.marginLeft = "-300px"
             $("#header").fadeOut();
             if ($("#header").is(":hidden")) {
                 document.getElementById("bod").style.marginLeft = "0px"
                 $("#header").fadeIn();
-
             }
-        }
+      });
+      
+  })(jQuery);
