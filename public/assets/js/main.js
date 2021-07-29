699be15dd3caac5265fa818521c835ab23818a76
m-pad-studios <mpadinds@gmail.com>
2021-07-29 12:51:44 -0700

    -c

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -176,3 +176,10 @@
   });
 
 })(jQuery);
+
+//Goog Tag Manager
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-70K0QDFJZW');
